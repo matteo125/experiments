4e99409d864f157b685875dec92871ca558af64b
Matteo Revelli <matteo.revelli@mikamai.com>
2017-01-17 17:18:53 +0100

    add root page and schema db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :to_dos
+  root 'to_dos#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
