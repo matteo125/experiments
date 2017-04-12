ffceb538a3bd95bc02fe2f1fcbc8ceb00625c07a
Matteo Revelli <matteo.revelli@mikamai.com>
2017-04-12 10:51:57 +0200

    continuazioni varie

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,11 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :title, length: { maximum: 30 }
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 500 }
+
+  # before_save :assign_user_id, on: :create
+
+  # def self.assign_user_id
+  #   self.user_id = current_user.id
+  # end
 end
